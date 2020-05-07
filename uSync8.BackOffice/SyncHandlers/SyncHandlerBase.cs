e245dc4c34851496d4d7cb43adc31f4ded021a3b
Kevin Jump <kevin@jumoo.co.uk>
2020-05-07 10:40:21 +0100

    re-vert the file change check into the handler base.

--- uSync8.BackOffice/SyncHandlers/SyncHandlerBase.cs
+++ uSync8.BackOffice/SyncHandlers/SyncHandlerBase.cs
@@ -892,26 +892,33 @@ namespace uSync8.BackOffice.SyncHandlers
 
             foreach (string file in files)
             {
-                if (!file.InvariantEquals(physicalFile))
+                if (!syncFileService.PathMatches(physicalFile, file))
                 {
-                    var node = syncFileService.LoadXElement(file);
-
-                    // if this xml file matches the item we have just saved. 
-
-                    if (!node.IsEmptyItem() || node.GetEmptyAction() != SyncActionType.Rename)
+                    try
                     {
-                        // the node isn't empty, or its not a rename (because all clashes become renames)
+                        var node = syncFileService.LoadXElement(file);
 
-                        if (DoItemsMatch(node, item))
+                        // if this xml file matches the item we have just saved. 
+
+                        if (!node.IsEmptyItem() || node.GetEmptyAction() != SyncActionType.Rename)
                         {
-                            logger.Debug(handlerType, "Duplicate {file} of {alias}, saving as rename", Path.GetFileName(file), this.GetItemAlias(item));
+                            // the node isn't empty, or its not a rename (because all clashes become renames)
 
-                            var attempt = serializer.SerializeEmpty(item, SyncActionType.Rename, node.GetAlias());
-                            if (attempt.Success)
+                            if (DoItemsMatch(node, item))
                             {
-                                syncFileService.SaveXElement(attempt.Item, file);
+                                logger.Debug(handlerType, "Duplicate {file} of {alias}, saving as rename", Path.GetFileName(file), this.GetItemAlias(item));
+
+                                var attempt = serializer.SerializeEmpty(item, SyncActionType.Rename, node.GetAlias());
+                                if (attempt.Success)
+                                {
+                                    syncFileService.SaveXElement(attempt.Item, file);
+                                }
                             }
                         }
+                    }
+                    catch(Exception ex)
+                    {
+                        logger.Warn(handlerType, "Failed to cleanup file: {file} {error}", file, ex.Message);
                     }
                 }
             }
